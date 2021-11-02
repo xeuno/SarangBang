5ddb616f4f7e71d6a37173a2ecf1a96f3d2d604b
SeongeunYang <tnvjaos1826@gmail.com>
2021-11-02 21:38:53 +0900

    폴더 구조 확정. 헤더, 푸터 고정. 글작성 페이지까지 합침. div 겹침문제 해결 필요!

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.cafes
 
-# HTML 화면 보여주기
+## HTML 화면 보여주기
 @app.route('/')
 def homework():
+    return render_template('layout.html')
+
+@app.route('/writing')
+def layout_writing():
     return render_template('layout_writing.html')
 
 # 저장하기(POST) API
-@app.route('/api/writing', methods=['POST'])
+@app.route('/writing', methods=['POST'])
 def save_order():
     # 클라이언트로 부터 정보 넘겨받기
     cafe_name = request.form['cafe_name']
---
+++ static/js/layout_writing.js
@@ -0,0 +1,52 @@
+function imgToBase64ByFileReader(url) {
+            return new Promise((resolve, reject) => {
+                let xhr = new XMLHttpRequest();
+                xhr.onload = () => {
+                    let reader = new FileReader();
+                    reader.onloadend = function () {
+                        resolve(reader.result);
+                        document.getElementById('img-id').setAttribute('src', reader.result);
+                    }
+                    reader.readAsDataURL(xhr.response);
+                }
+                xhr.open('GET', url);
+                xhr.responseType = 'blob';
+                xhr.send();
+            });
+        }
+
+        function loadFile(input) {
+            let file = input.files[0]; // 선택된 파일 가져오기
+
+            let newImage = document.createElement("img"); //새 이미지 추가
+            //이미지 source 가져오기
+            newImage.src = URL.createObjectURL(file);
+            newImage.id = "img-id"
+            newImage.style.width = "100%";
+            newImage.style.height = "100%";
+            newImage.style.objectFit = "cover";
+
+            //이미지를 image-show div에 추가
+            let container = document.getElementById('image-show');
+            container.appendChild(newImage);
+
+            //이미지를 서버에 저장하기 위해 base64 형태로 변환
+            imgToBase64ByFileReader($('#img-id').attr("src"));
+        }
+
+        function writing() {
+            $.ajax({
+                type: "POST",
+                url: "/api/writing",
+                data: {
+                    cafe_name: $('#cafeName').val(),
+                    cafe_address: $('#cafeAddress').val(),
+                    cafe_coment: $('#cafeComent').val(),
+                    cafe_img: $('#img-id').attr("src")
+                },
+                success: function (response) { // 성공하면
+                    alert(response["msg"]);
+                    window.location.reload();
+                }
+            })
+        }
